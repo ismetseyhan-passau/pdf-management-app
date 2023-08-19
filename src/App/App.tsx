84683894ad583dff06ef39fb9a82764468e2d82f
ismetseyhan <ismet@capo.build>
2023-08-19 23:49:35 +0200

    login register pages added

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import '../App.css'
+import SignIn from "../components/SignIn.tsx";
+
+function App() {
+
+    return (
+        <>
+            <SignIn></SignIn>
+        </>
+    )
+}
+
+export default App
